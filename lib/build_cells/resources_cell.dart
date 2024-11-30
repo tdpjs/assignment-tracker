abc1a226d711113b2ba198d4783cb256c02dddcf
Dat Pham <phamtatdat1510@gmail.com>
2024-11-29 16:28:28 -0800

    Edit resources cell's text color

--- lib/build_cells/resources_cell.dart
+++ lib/build_cells/resources_cell.dart
@@ -2,44 +2,75 @@ import 'package:flutter/material.dart';
 import '../dialogs/show_cell_dialog.dart';
 import 'link_cell.dart';
 
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 class ResourcesCell extends DataCell {
   ResourcesCell({
     required String name,
     required List<dynamic> resources,
     required BuildContext context,
-  }) : super( (resources.isNotEmpty) ?
-      GestureDetector(
-        onLongPress: () => showResourcesDialog(context, 'Resources', resources.whereType<String>().toList()),
-        onDoubleTap: () => showResourcesDialog(context, 'Resources', resources.whereType<String>().toList()),
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 300, maxHeight: 120),
-          child: SingleChildScrollView(
-            child: Column(
-              children: resources
-                  .whereType<String>()
-                  .map<Widget>((resource) => Padding(
+  }) : super(
+    resources.isNotEmpty
+        ? GestureDetector(
+      onLongPress: () => showResourcesDialog(
+        context,
+        'Resources',
+        resources.whereType<String>().toList(),
+      ),
+      onDoubleTap: () => showResourcesDialog(
+        context,
+        'Resources',
+        resources.whereType<String>().toList(),
+      ),
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 300, maxHeight: 120),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: resources
+                .whereType<String>()
+                .map<Widget>(
+                  (resource) => Padding(
                 padding: const EdgeInsets.symmetric(vertical: 2.0),
-                child: GestureDetector(
-                  onLongPress: () => showResourcesDialog(
-                      context, 'Resources', [resource]),
-                  onDoubleTap: () => showResourcesDialog(
-                      context, 'Resources', [resource]),
-                  child: Text(
-                    resource,
+                child: Uri.tryParse(resource)?.hasAbsolutePath ?? false
+                    ? RichText(
+                  text: TextSpan(
+                    text: resource,
                     style: const TextStyle(
                       color: Colors.blue,
                       decoration: TextDecoration.underline,
                     ),
-                    textAlign: TextAlign.center,
-                    overflow: TextOverflow.ellipsis,
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () async {
+                        final Uri uri = Uri.parse(resource);
+                        if (await canLaunchUrl(uri)) {
+                          await launchUrl(uri, mode: LaunchMode.externalApplication);
+                        } else {
+                          print('Could not launch $uri');
+                        }
+                      },
                   ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                )
+                    : Text(
+                  resource,
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
                 ),
-              ))
-                  .toList(),
-            ),
+              ),
+            )
+                .toList(),
           ),
         ),
-      ) : const SelectableText('-')
+      ),
+    )
+        : const SelectableText('-'),
   );
 }
 
